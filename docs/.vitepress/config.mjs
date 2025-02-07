ea9fe07a52988f028be3ed1c4a9e7c80af037f65
aw07734 <53339912+aw07734@users.noreply.github.com>
2025-02-07 17:46:47 +0800

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,20 +2,6 @@ import { defineConfig } from 'vitepress'
 
 import mulu from './mulu.mjs'
 
-var segment = new Segment();
-segment.useDefault();
-
-const options = {
-  encode: function (str) {
-    return segment.doSegment(str, { simple: true });
-  },
-  tokenize: "forward",
-  previewLength: 62,
-  buttonLabel: "Search",
-  placeholder: "Search docs",
-  allow: [],
-  ignore: [],
-};
 export default defineConfig({
   title: "红龙贼合集",
   description: "A VitePress Site",
